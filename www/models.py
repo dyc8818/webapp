db5a6e71cdc652ea06308a53e924385532556162
dylc1 <387768965@qq.com>
2017-08-05 10:46:33 +0800

    5th day coreweb

---
+++ www/models.py
@@ -0,0 +1,53 @@
+from orm import Model,BooleanField,StringField,IntegerField,TextField,FloatField
+import uuid,time
+
+def next_id():
+    return '%015d%s000' % (int(time.time()*1000),uuid.uuid4().hex)
+
+class User(Model):
+    __table__ = 'users'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_at = FloatField(default=time.time)
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+class Comment(Model):
+    __table__ = 'comments'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+if __name__ == '__main__':
+    import orm,asyncio
+    from models import User,Blog,Comment
+
+    loop= asyncio.get_event_loop()
+
+    async def test():
+        await orm.create_pool(loop,user='webapp',password='app',db='awesome')
+        u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+        await u.save()
+
+    loop.run_until_complete(test())
+    loop.close()
